d5bed20c0bd28b7c692caaace55c424e6e664774
KHilse <khilsedev@gmail.com>
2019-09-10 11:46:33 -0700

    GET and PUT routes created

--- controllers/v1/bounties.js
+++ controllers/v1/bounties.js
@@ -15,6 +15,36 @@ router.get("/", (req, res) => {
 })
 
 
+// GET /v1/bounties/:id
+router.get("/:id", (req, res) => {
+	res.send("STUB for GET /bounties/id");
+})
 
+// POST /v1/bounties
+router.post("/", (req, res) => {
+	db.Bounty.create(req.body)
+	.then(bounty => {
+		res.status(201).send(bounty);
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(500).send("ERROR creating new bounty");
+	})
+})
+
+// PUT /v1/bounties/:id
+router.put("/:id", (req, res) => {
+	res.send("STUB for PUT /bounties/id");
+})
+
+// DELETE /v1/bounties (delete all)
+router.delete("/", (req, res) => {
+	res.send("STUB for DELETE /bounties (all)");
+})
+
+// DELETE /v1/bounties/:id (delete one)
+router.delete("/:id", (req, res) => {
+	res.send("STUB for DELETE /bounties/id");
+})
 
 module.exports = router;
