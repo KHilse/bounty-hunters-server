565c60ff9e735c3fce840cf0a0ef9ae93f306fe6
KHilse <khilsedev@gmail.com>
2019-09-10 12:34:43 -0700

    Added remaining routes

--- controllers/v1/bounties.js
+++ controllers/v1/bounties.js
@@ -14,10 +14,20 @@ router.get("/", (req, res) => {
 	})
 })
 
-
 // GET /v1/bounties/:id
 router.get("/:id", (req, res) => {
-	res.send("STUB for GET /bounties/id");
+	db.Bounty.findById(req.params.id)
+	.then(bounty => {
+		if (bounty) {
+		res.send(bounty);			
+		} else {
+			res.status(404).send({ message: 'Resource not located'});
+		}
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(503).send({ message: 'ERROR finding bounty by id' });
+	})
 })
 
 // POST /v1/bounties
@@ -27,24 +37,54 @@ router.post("/", (req, res) => {
 		res.status(201).send(bounty);
 	})
 	.catch(err => {
-		console.log(err);
-		res.status(500).send("ERROR creating new bounty");
+		console.log(err.name);
+		if (err.name === 'ValidationError') {
+			res.status(406).send({ message: 'Validation Error'});
+		} else {
+			res.status(503).send({ message: 'Database or server error' });
+		}
 	})
 })
 
 // PUT /v1/bounties/:id
 router.put("/:id", (req, res) => {
-	res.send("STUB for PUT /bounties/id");
+	db.Bounty.findOneAndUpdate({
+		_id: req.params.id
+		}, req.body,
+		{new: true}
+	)
+	.then(editedBounty => {
+		res.send(editedBounty);
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(503).send("ERROR editing bounty");
+	})
+
 })
 
 // DELETE /v1/bounties (delete all)
 router.delete("/", (req, res) => {
-	res.send("STUB for DELETE /bounties (all)");
+	db.Bounty.remove()
+	.then(() => {
+		res.send({ message: 'Deleted all records'});
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(503).send({ message: 'Server error while deleting'});
+	})
 })
 
 // DELETE /v1/bounties/:id (delete one)
 router.delete("/:id", (req, res) => {
-	res.send("STUB for DELETE /bounties/id");
+	db.Bounty.findByIdAndDelete(req.params.id)
+	.then(() => {
+		res.status(204).send();
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(503).send({ message: 'Server error while attempting delete'});
+	})
 })
 
 module.exports = router;
